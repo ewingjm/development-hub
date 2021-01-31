cf4215606be50c6562ca2423148754fc7c1ae04b
Max Ewing <max.ewing@capgemini.com>
2021-01-31 20:36:53 +0700

    ci: fix path in Set-SolutionVersion.ps1 script

--- scripts/Set-SolutionVersion.ps1
+++ scripts/Set-SolutionVersion.ps1
@@ -1,15 +1,15 @@
 [CmdletBinding()]
 param (
-    [Parameter(Mandatory = $true)]
+    [Parameter(Mandatory = $false)]
     [String[]]
-    $Solutions,
-    [Parameter(Mandatory = $true)]
+    $Solutions = "devhub_DevelopmentHub_Issues",
+    [Parameter(Mandatory = $false)]
     [String]
-    $Version
+    $Version = "0.1.6.2"
 )
 
 $Solutions | ForEach-Object {
-    $solutionPath = Join-Path -Path .\solutions -ChildPath $_
+    $solutionPath = Join-Path -Path .\src\solutions -ChildPath $_
     $solutionXmlPath = Join-Path -Path $solutionPath -ChildPath '.\Extract\Other\Solution.xml'
     
     [xml]$solutionXml = Get-Content -Path $solutionXmlPath
